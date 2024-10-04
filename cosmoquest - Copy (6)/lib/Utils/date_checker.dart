9613b95cac36e45c329c10af95cf95e0dd4e16b8
Nazibullah Noyon <145152033+noyon-360@users.noreply.github.com>
2024-10-05 02:47:17 +0600

    Delete cosmoquest - Copy (6) directory

--- cosmoquest - Copy (6)/lib/Utils/date_checker.dart
+++
@@ -1,6 +0,0 @@
-extension DateTimeExtensions on DateTime {
-  bool isToday() {
-    final now = DateTime.now();
-    return this.year == now.year && this.month == now.month && this.day == now.day;
-  }
-}
