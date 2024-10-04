b9141ea23dc0dd8d763527ed19610777b9fd1dfd
Nazibullah Noyon <145152033+noyon-360@users.noreply.github.com>
2024-10-05 02:48:40 +0600

    Delete cosmoquest - Copy (2) directory

--- cosmoquest - Copy (2)/lib/Utils/Game_2/game_contants.dart
+++
@@ -1,8 +0,0 @@
-class GameTypes {
-  static const String triviaQuiz = "Trivia Quiz";
-  static const String matchingGame = "Matching Game";
-  static const String puzzleChallenge = "Puzzle Challenge";
-  static const String memoryGame = "Memory Game";
-  static const String exoplanetBuilder = "Exoplanet Builder";
-  static const String spaceAdventureGame = "Space Adventure Game";
-}
